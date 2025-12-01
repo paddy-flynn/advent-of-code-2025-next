093b7aad1bf2e3f08c9500f82e632f822f858688
Patrick Flynn <pflynn210392@gmail.com>
2025-12-01 10:55:45 +0000

    Adding analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import "@/styles/globals.css";
 import { Providers } from "./providers";
 import { Metadata } from "next";
+import { Analytics } from "@vercel/analytics/next";
 
 export const metadata: Metadata = {
   title: "Advent of Code",
@@ -40,6 +41,7 @@ export default function RootLayout({
     <html lang="en">
       <body>
         <Providers>{children}</Providers>
+        <Analytics />
       </body>
     </html>
   );
